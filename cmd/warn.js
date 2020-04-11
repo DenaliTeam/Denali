2ff731a898b453cae726415dd8c6ca8a06571bb0
Wing <mcprowing@gmail.com>
2020-04-10 20:43:28 -0700

    -warn command

--- cmd/warn.js
+++ cmd/warn.js
@@ -1,11 +1,15 @@
 exports.run = (client, message, args) => {
-    message.content.slice(6, message.content.length)
+    var warning = message.content.slice(6, message.content.length)
     client.guilds.forEach(g => {
         var ch = client.net.get(g.id, 'alertchannel')
         g.channels.get(ch).send({
             embed:{
-                title:'Warning',
-                description: ``
+                title:'Warning:',
+                description: '```' + warning + '```'
+            },
+            'footer':{
+                'text': `From ${message.guild.name}`,
+                'iconURL': message.guild.iconURL
             }
         })
     });
