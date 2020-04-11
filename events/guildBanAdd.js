3053368fe5363db27c13166c006c5ab3171966ca
Wing <mcprowing@gmail.com>
2020-04-10 20:05:11 -0700

    'banlog'

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,3 +1,13 @@
 module.exports = (client, guild, user) => {
-    
+    client.net.ensure('bans-' + user.id, {
+        'name':'',
+        'guilds':'',
+        'reasons':''
+    })
+
+    var guilds = client.net.get('bans-' + user.id, 'guilds')
+    client.net.set('bans-' + user.id, guilds + guild.id + ', ', 'guilds')
+    client.net.set('bans-' + user.id, user.tag, 'name')
+
+
 }
