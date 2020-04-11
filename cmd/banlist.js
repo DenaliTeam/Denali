1d2531aeec7eb9eb3cbb63555de60a29778795ce
Wing <mcprowing@gmail.com>
2020-04-10 20:10:07 -0700

    Added banlist command

---
+++ cmd/banlist.js
@@ -0,0 +1,23 @@
+exports.run = (client, message, args) => {
+    var list = ''
+    console.log(client.net)
+    var u = message.mentions.users.first() || args[0]
+        if(typeof u == 'undefined') return
+        console.log(args[0])
+    
+       var bans = client.net.get(`bans-${u.id || args[0]}`, 'guilds').split(', ')
+       var name = client.net.get(`bans-${u.id || args[0]}`, 'name')
+        bans.forEach(b => {
+            if(b.length < 2) return
+            g = client.guilds.get(b)
+            list = list + `**${g.name}**\n`
+        });
+    
+    if(list.length < 1) return
+    message.channel.send({
+        embed:{
+            title: `${name}'s bans`,
+            description: list
+        }
+    })
+}
